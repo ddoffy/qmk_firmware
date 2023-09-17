175ff076dbc59b35d069b63063fe9b92adf7fc74
D. Doffy <cuongnsm@gmail.com>
2023-09-17 07:33:30 +0700

    Add Doffy logo, update rules for encoder map

---
+++ keyboards/sofle/keymaps/noqmk/config.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// OLED settings
+#define OLED_TIMEOUT        80000
+#define OLED_BRIGHTNESS     90
+#define OLED_DISPLAY_CLOCK  0x80
+
+#define SPLIT_WPM_ENABLE
+#define SPLIT_OLED_ENABLE
+#define SPLIT_TRANSPORT_MIRROR
