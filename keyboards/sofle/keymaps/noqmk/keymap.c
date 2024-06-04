c300e4891da5616abb8519ebf6afe21bea527d19
D. Doffy <nguyen.symanhcuong@trader.ca>
2024-06-03 18:46:20 -0600

    Add quot mark and ins for easier using in linux

--- keyboards/sofle/keymaps/noqmk/keymap.c
+++ keyboards/sofle/keymaps/noqmk/keymap.c
@@ -95,7 +95,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
   KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
   _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LBRC, KC_LCBR, _______,    _______, KC_RCBR, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,    _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
                     _______, _______, _______, _______, KC_SPC,        KC_SPC, _______, _______, _______, _______
 ),
 /* RAISE
@@ -106,7 +106,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | Tab  |  ae  |  ue  |  oe  |  ss  | euro |-------.    ,-------| Left | Down |  Up  | Rigth|  Del | Bspc |
  * |------+------+------+------+------+------|  MUTE  |   |       |------+------+------+------+------+------|
- * |Shift | Undo |  Cut | Copy | Paste| caps |-------|    |-------|      | LStr |      | LEnd |      | Shift|
+ * |Shift | Undo |  Cut | Copy | Paste| caps |-------|    |-------|   '  | Ins |      | LEnd |      | Shift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | / Space /       \Space \  |RAISE | RGUI | RAlt | RCTR |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -115,9 +115,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_RAISE] = LAYOUT(
   _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
   _______,KC_INS,  KC_PSCR,KC_MENU,  LGUI(KC_4), LGUI(KC_5),                        LGUI(KC_6), KC_PGDN,   KC_PGUP, LGUI(KC_9),LGUI(KC_0), KC_BSPC,
-  _______,RALT(KC_Q), RALT(KC_Y),  RALT(KC_P), RALT(KC_S) , RALT(KC_5),                       KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, KC_CAPS,  _______,       _______,  XXXXXXX, KC_LSTRT, XXXXXXX, KC_LEND,   XXXXXXX, _______,
-                    _______, _______, _______, _______, KC_SPC,       KC_SPC, _______, _______, _______, _______
+  _______,RALT(KC_Q), RALT(KC_Y),  RALT(KC_P), RALT(KC_S) , RALT(KC_5),             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, KC_CAPS,  _______,            _______, KC_QUOT, KC_INS, XXXXXXX, KC_LEND,   XXXXXXX, _______,
+                    _______, _______, _______, _______, KC_SPC,                     KC_SPC, _______, _______, _______, _______
 ),
 /*
  * QWERTY
