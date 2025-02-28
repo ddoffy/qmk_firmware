b4eecf117bf583d4710ef2dc0fd9ff512760e8b3
D. Doffy <cuongnsm@gmail.com>
2025-02-28 14:52:57 -0600

    Swap encoder button and add media keys to the left

--- keyboards/sofle/keymaps/noqmk/keymap.c
+++ keyboards/sofle/keymaps/noqmk/keymap.c
@@ -4,7 +4,6 @@
 
 #define MOD_SPC MT(MOD_LGUI, KC_SPC)
 
-
 /*
 keymap_config.swap_lctl_lgui = true; it means the keyboard is in MAC mode
  */
@@ -75,7 +74,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_SW,   KC_TAB1,  KC_TAB2, KC_TAB3, KC_TAB4, KC_TAB5,                   KC_TAB6, KC_TAB7, KC_TAB8,  KC_TAB9, KC_0, TD(TD_GRV_QUOT),
   KC_ESC,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,  KC_I,   KC_O,  KC_P,  KC_BSPC,
   KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,  KC_K,   KC_L, KC_SCLN,  KC_ENT,
-  KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_NEWTAB,      KC_MUTE, KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
+  KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       KC_NEWTAB, KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
             KC_LGUI,   KC_LALT,  KC_LCTL, KC_LOWER, MOD_SPC,                 KC_SPC, KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* LOWER
@@ -876,7 +875,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 #if defined(ENCODER_MAP_ENABLE)
     const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
         [_QWERTY] = {
-            ENCODER_CCW_CW(KC_MS_WH_UP, KC_MS_WH_DOWN),
+            ENCODER_CCW_CW(KC_VOLU, KC_VOLD),
+            ENCODER_CCW_CW(KC_PGUP, KC_PGDN)
         },
         [_LOWER] = {
             ENCODER_CCW_CW(RGB_HUD, RGB_HUI),
