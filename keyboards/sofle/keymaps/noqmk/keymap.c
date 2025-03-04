3f0c061cd55e20805203fec6882fa8f007aceee4
D. Doffy <cuongnsm@gmail.com>
2025-03-03 18:52:35 -0600

    Revert to original space

--- keyboards/sofle/keymaps/noqmk/keymap.c
+++ keyboards/sofle/keymaps/noqmk/keymap.c
@@ -2,8 +2,6 @@
 // SPDX-License-Identifier: GPL-2.0-only
 #include QMK_KEYBOARD_H
 
-#define MOD_SPC MT(MOD_LGUI, KC_SPC)
-
 /*
 keymap_config.swap_lctl_lgui = true; it means the keyboard is in MAC mode
  */
@@ -75,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,  KC_I,   KC_O,  KC_P,  KC_BSPC,
   KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,  KC_K,   KC_L, KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       KC_NEWTAB, KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-            KC_LGUI,   KC_LALT,  KC_LCTL, KC_LOWER, MOD_SPC,                 KC_SPC, KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI
+            KC_LGUI,   KC_LALT,  KC_LCTL, KC_LOWER, KC_SPC,                 KC_SPC, KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
