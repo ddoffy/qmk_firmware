39911899e2e7b223a0615c84ea7435b574ad5091
D. Doffy <cuongnsm@gmail.com>
2025-03-31 07:39:33 -0600

    Change layout as corne

--- keyboards/sofle/keymaps/noqmk/keymap.c
+++ keyboards/sofle/keymaps/noqmk/keymap.c
@@ -73,17 +73,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,   KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,                       KC_Y,    KC_U,  KC_I,   KC_O,  KC_P,  KC_BSPC,
   KC_TAB,   KC_A,     KC_S,    KC_D,    KC_F,    KC_G,                       KC_H,    KC_J,  KC_K,   KC_L, KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,     KC_X,    KC_C,    KC_V,    KC_B, KC_MUTE,       KC_NEWTAB, KC_N, KC_M, KC_COMM,  KC_DOT, KC_SLSH,  KC_RSFT,
-            KC_LGUI,   KC_LALT,  KC_LCTL, KC_LOWER, KC_SPC,                 KC_SPC, KC_RAISE, KC_RCTL, KC_RALT, KC_RGUI
+            KC_LGUI,   KC_LALT,  KC_LCTL, KC_LOWER, KC_SPC,                 KC_SPC, KC_RAISE, KC_RALT, KC_RCTL, KC_RGUI
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
  * |  `   |  F1  |  F2  |  F3  |  F4  |  F5  |                    |  F6  |  F7  |  F8  |  F9  | F10  | F11  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | ESC  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | F12  |
+ * |   `  |   1  |   2  |   3  |   4  |   5  |                    |   6  |   7  |   8  |   9  |   0  | DEL  |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
+ * |   ~  |   !  |   @  |   #  |   $  |   %  |-------.    ,-------|   ^  |   &  |   *  |   (  |   )  |   |  |
  * |------+------+------+------+------+------|New_tab|    |S_wndw|------+------+------+------+------+------|
- * | Shift|  =   |  -   |  +   |   [  |   {  |-------|    |-------|   }  |   ]  |   ;  |   :  |   \  | Shift|
+ * |  _   |  =   |  -   |  +   |   [  |   {  |-------|    |-------|   }  |   ]  |   '  |   "  |   \  | Shift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR | Lower | / Space /       \Space\   |Raise | RCTR | RAlt | RGUI |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -91,20 +91,20 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,  KC_F10,  KC_F11,
-  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_F12,
-  _______, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
-  _______,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,    _______, KC_LBRC, KC_RBRC, KC_SCLN, KC_COLN, KC_BSLS, _______,
+  KC_GRV,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                       KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
+  KC_TILD, KC_EXLM,   KC_AT, KC_HASH,  KC_DLR, KC_PERC,                      KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_PIPE,
+  KC_UNDS,  KC_EQL, KC_MINS, KC_PLUS, KC_LCBR, KC_RCBR, _______,    _______, KC_LBRC, KC_RBRC, KC_QUOT, KC_DQUO, KC_BSLS, KC_RSFT,
                     _______, _______, _______, _______, KC_SPC,        KC_SPC, _______, _______, _______, _______
 ),
 /* RAISE
  * ,----------------------------------------.                    ,-----------------------------------------.
  * |  `   |      |      |      |      |      |                    |           |      |      |      |      |      |
  * |------+------+------+------+------+------|                    |------     +------+------+------+------+------|
- * | Esc  | Ins  | Pscr | Menu |SSHOT |      |                    |           | PWrd |      | NWrd | DLine| Bspc |
+ * | Esc  |  F1  |   F2 |   F3 |   F4 |  F5  |                    |           | PWrd |      | NWrd | DLine| Bspc |
  * |------+------+------+------+------+------|                    |------     +------+------+------+------+------|
- * | Tab  |  ae  |  ue  |  oe  |  ss  | euro |-------.    ,-------| Left      | Down |  Up  | Rigth|  Del | Bspc |
+ * | Tab  |  F6  |  F7  |  F8  |  F9  |  F10 |-------.    ,-------| Left      | Down |  Up  | Rigth|  Del | Bspc |
  * |------+------+------+------+------+------|  MUTE  |   |       |------+------+------+------+------+------|
- * |Shift | Undo |  Cut | Copy | Paste| caps |-------|    |-------| KC_CREATE |    ' |  Ins | LEnd |      | Shift|
+ * |Shift | Undo |  Cut | Copy | F11  |  F12 |-------|    |-------| KC_CREATE |    ' |  Ins | LEnd |      | Shift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
  *            | LGUI | LAlt | LCTR |LOWER | / Space /       \Space \  |RAISE | RGUI | RAlt | RCTR |
  *            |      |      |      |      |/       /         \      \ |      |      |      |      |
@@ -112,9 +112,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_RAISE] = LAYOUT(
   _______, _______ , _______ , _______ , _______ , _______,                           _______,  _______  , _______,  _______ ,  _______ ,_______,
-  _______,KC_INS,  KC_PSCR,KC_MENU,KC_SCREENSHOT, LGUI(KC_5),                        LGUI(KC_6), KC_PGDN,   KC_PGUP, LGUI(KC_9),LGUI(KC_0), KC_BSPC,
-  _______,RALT(KC_Q), RALT(KC_Y),  RALT(KC_P), RALT(KC_S) , RALT(KC_5),             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
-  _______,KC_UNDO, KC_CUT, KC_COPY, KC_PASTE, KC_CAPS,  _______,            _______,KC_CREATE, KC_QUOT, KC_INS, KC_LEND,   XXXXXXX, _______,
+  _______,KC_F1,  KC_F2,KC_F3,KC_F4, KC_F5,                        LGUI(KC_6), KC_PGDN,   KC_PGUP, LGUI(KC_9),LGUI(KC_0), KC_BSPC,
+  _______,KC_F6,  KC_F7, KC_F8, KC_F9 , KC_F10,             KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  KC_DEL, KC_BSPC,
+  _______,KC_UNDO, KC_CUT, KC_PSCR, KC_F11, KC_F12,  _______,            _______,KC_CREATE, KC_QUOT, KC_INS, KC_LEND,   XXXXXXX, _______,
                     _______, _______, _______, _______, KC_SPC,                     KC_SPC, _______, _______, _______, _______
 ),
 /* ADJUST
