c49646173d59d9aa7c7cadb01e994f4a41851cb8
D. Doffy <cuongnsm@gmail.com>
2025-04-13 23:56:16 -0600

    Add some media keys

--- keyboards/sofle/keymaps/noqmk/keymap.c
+++ keyboards/sofle/keymaps/noqmk/keymap.c
@@ -49,10 +49,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 [_QWERTY] = LAYOUT(
   _______,   _______,    _______,      _______,     _______,      _______,                        _______,     _______,  _______,  _______,  _______, _______,
-  QK_GESC,  KC_Q,        KC_W,         KC_E,        KC_R,         KC_T,                           KC_Y,        KC_U,         KC_I,  KC_O, KC_P, KC_EQL,
+  QK_GESC,  KC_Q,        KC_W,         KC_E,        KC_R,         KC_T,                           KC_Y,        KC_U,         KC_I,  KC_O, KC_P, KC_UNDS,
   KC_BSPC,  LGUI_T(KC_A), LALT_T(KC_S), CTL_T(KC_D), LSFT_T(KC_F), KC_G,                           KC_H, RSFT_T(KC_J), RCTL_T(KC_K), RALT_T(KC_L), RGUI_T(KC_SCLN), KC_QUOT,
   KC_PIPE,  KC_Z,        KC_X,         KC_C,        KC_V,         KC_B, KC_MUTE,       _______, KC_N,  KC_M,       KC_COMM,    KC_DOT,     KC_SLSH, KC_MINUS,
-            _______,     _______, KC_DEL,   LT(_LOWER, KC_TAB),  LT(_NAV, KC_SPC),                 LT(_FUN, KC_SPC), LT(_RAISE, KC_ENT), LT(_ADJUST,KC_EQL), _______, _______
+            _______,     _______, LT(_ADJUST, KC_EQL),   LT(_LOWER, KC_TAB),  LT(_NAV, KC_SPC),                 LT(_FUN, KC_SPC), LT(_RAISE, KC_ENT), KC_DEL, _______, _______
 ),
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
@@ -121,9 +121,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_ADJUST] = LAYOUT(
   XXXXXXX , XXXXXXX,  XXXXXXX ,  XXXXXXX , XXXXXXX, XXXXXXX,                     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
-  QK_BOOT  , XXXXXXX,KC_QWERTY,XXXXXXX,CG_TOGG,XXXXXXX,                     XXXXXXX, MS_BTN1, MS_BTN3, MS_BTN2, XXXXXXX, XXXXXXX,
+  QK_BOOT  , XXXXXXX,KC_QWERTY,XXXXXXX,CG_TOGG,XXXXXXX,                     XXXXXXX, MS_BTN1, MS_BTN3, MS_BTN2, XXXXXXX, KC_SLEP,
   XXXXXXX , XXXXXXX,CG_TOGG, XXXXXXX,    XXXXXXX,  XXXXXXX,                     MS_LEFT, MS_DOWN, MS_UP, MS_RGHT, XXXXXXX, XXXXXXX,
-  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, XXXXXXX, KC_MPRV, KC_MPLY, KC_MNXT, XXXXXXX, XXXXXXX,
+  XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,    XXXXXXX,  XXXXXXX, XXXXXXX,     XXXXXXX, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT, KC_MPRV, KC_MUTE,
                           _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
 ),
 [_FUN] = LAYOUT(
