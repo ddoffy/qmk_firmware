212d2d4bf380774a54aaf287d1a9f8b7532b308b
D. Doffy <cuongnsm@gmail.com>
2025-04-17 00:55:10 -0600

    Init branch for my dactyl manuform 5x7

---
+++ keyboards/sofle/keymaps/dactyl/config.h
@@ -0,0 +1,26 @@
+// Copyright 2024 Santosh Kumar (@santosh)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define TRI_LAYER_LOWER_LAYER 2
+#define TRI_LAYER_UPPER_LAYER 3
+#define TRI_LAYER_ADJUST_LAYER 4
+
+#define SPLIT_TRANSPORT_MIRROR
+
+#define MASTER_LEFT
+
+#ifdef MASTER_LEFT
+#define USE_SERIAL
+#define EE_HANDS
+#endif
+
+#ifdef MASTER_RIGHT
+#define USE_SERIAL
+#define EE_HANDS
+#endif
+
+#define SERIAL_PIN D0
+// Optionally add
+#define SPLIT_USB_DETECT //later if needed
---
+++ keyboards/sofle/keymaps/dactyl/rules.mk
@@ -0,0 +1,20 @@
+TRI_LAYER_ENABLE = yes
+
+OLED_ENABLE = no
+ENCODER_ENABLE = no
+ENCODER_MAP_ENABLE = no
+EXTRAKEY_ENABLE = yes
+WPM_ENABLE = no
+BOOTMAGIC_ENABLE = no       # Enable Bootmagic Lite
+MOUSEKEY_ENABLE = yes        # Mouse keys
+CONSOLE_ENABLE = no         # Console for debug
+COMMAND_ENABLE = no         # Commands for debug and configuration
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+RGBLIGHT_ENABLE = no        # Enable keyboard RGB underglow
+AUDIO_ENABLE = no           # Audio output
+LTO_ENABLE = yes
+SPLIT_KEYBOARD = yes
+TAP_DANCE_ENABLE = no
+COMBO_ENABLE = no
+SERIAL_USART_HALF_DUPLEX = yes  # Likely needed for single wire
