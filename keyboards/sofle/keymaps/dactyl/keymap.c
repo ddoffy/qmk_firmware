0e2872be55b2b9480708e62af29f97edf3f36ffb
D. Doffy <cuongnsm@gmail.com>
2025-04-23 08:51:10 -0600

    Add capslock for dactyl

--- keyboards/sofle/keymaps/dactyl/keymap.c
+++ keyboards/sofle/keymaps/dactyl/keymap.c
@@ -82,7 +82,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
 */
 [_RAISE] = LAYOUT(
-  _______,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_0,                   KC_0,  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,     KC_F11,
+  _______,  KC_F1,   KC_F2,   KC_F3,    KC_F4,   KC_F5,   KC_CAPS,                KC_0,  KC_F6,    KC_F7,    KC_F8,    KC_F9,     KC_F10,     KC_F11,
   KC_GRV,   KC_1,    KC_2,    KC_3,     KC_4,    KC_5,    KC_0,                   KC_0,  KC_6,     KC_7,     KC_8,     KC_9,      KC_0,       KC_F12,
   KC_TILD,  KC_EXLM, KC_AT,   KC_HASH,  KC_DLR,  KC_PERC, KC_0,                   KC_0,  KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT,  KC_DEL,     KC_PIPE,
   KC_LSFT,  KC_EQL,  KC_MINS, KC_PLUS,  KC_LCBR, KC_RCBR,                                XXXXXXX,  KC_LSTRT, XXXXXXX,  KC_LEND,   XXXXXXX,    KC_RSFT,
