bc94c68b02bec41d8516f837ae95f0da488640f1
D. Doffy <cuongnsm@gmail.com>
2025-04-26 00:01:54 -0600

    change key map for convinient

--- keyboards/sofle/keymaps/dactyl/keymap.c
+++ keyboards/sofle/keymaps/dactyl/keymap.c
@@ -22,6 +22,7 @@ enum custom_keycodes {
     KC_LSTRT,
     KC_LEND,
     KC_TML, // TML = Tmux leader key Ctrl + a
+    KC_NEWT // new tab in tmux ctrl + a  c
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -41,14 +42,15 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 
+
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,        KC_5,   KC_PSCR,                     KC_MUTE, KC_6,    KC_7,       KC_8,    KC_9,    KC_0,     KC_QUOT,
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,        KC_5,   KC_CAPS,                     KC_MUTE, KC_6,    KC_7,       KC_8,    KC_9,    KC_0,     KC_QUOT,
   QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,        KC_T,   KC_PGUP,                     KC_VOLU, KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,     KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,        KC_G,   KC_PGDN,                     KC_VOLD, KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,        KC_B,                                         KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-  KC_UNDS,  KC_EQL, KC_MINS, KC_PLUS, MO(_LOWER),  KC_SPC,                                       KC_SPC,  MO(_RAISE), KC_TML,  KC_UNDS, KC_LBRC,  KC_RBRC,
-                                                KC_LCTL,   KC_LALT,                     KC_RALT,       KC_RCTL,
-                                                KC_LGUI,   MO(_ADJUST),                 KC_CAPS,       KC_RGUI
+  KC_UNDS,  KC_EQL, KC_LCTL, KC_LALT, MO(_LOWER),  LGUI_T(KC_SPC),                       RGUI_T(KC_SPC),  MO(_RAISE), KC_RALT, KC_RCTL, KC_LBRC,  KC_RBRC,
+                                                KC_MINS,   KC_PLUS,                     KC_QUOT,       KC_UNDS,
+                                                KC_PSCR,   MO(_ADJUST),                 KC_NEWT,       KC_TML
 ),
 
 /* LOWER
@@ -219,6 +221,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
         case KC_TML:
             SEND_STRING(SS_LCTL("a"));
+            break;
+
+        case KC_NEWT:
+            SEND_STRING(SS_LCTL("ac"));
             break;
 
     }
