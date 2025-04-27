b8c067abc1de24d6f5605b9ca6e5c6750a524b13
D. Doffy <cuongnsm@gmail.com>
2025-04-27 16:13:06 -0600

    Define some convinient keys for thumb cluster

--- keyboards/sofle/keymaps/dactyl/keymap.c
+++ keyboards/sofle/keymaps/dactyl/keymap.c
@@ -2,6 +2,11 @@
 // SPDX-License-Identifier: GPL-2.0-or-later
 #include QMK_KEYBOARD_H
 
+#define KC_ADJ_PSCR LT(_ADJUST, KC_PSCR)
+#define KC_RAISE MO(_RAISE)
+#define KC_LOWER MO(_LOWER)
+#define KC_ATL_QUOT LALT_T(KC_QUOT)
+
 const uint16_t PROGMEM esc_combo[] = {KC_Q, KC_W, COMBO_END};
 combo_t key_combos[] = {
     COMBO(esc_combo, KC_ESC),
@@ -41,15 +46,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  *            `----------------------------------'           '------''---------------------------'
  */
 
-
 [_QWERTY] = LAYOUT(
-  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,        KC_5,   KC_CAPS,                     KC_MUTE, KC_6,    KC_7,       KC_8,    KC_9,    KC_0,     KC_QUOT,
-  QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,        KC_T,   KC_PGUP,                     KC_VOLU, KC_Y,    KC_U,       KC_I,    KC_O,    KC_P,     KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,        KC_G,   KC_PGDN,                     KC_VOLD, KC_H,    KC_J,       KC_K,    KC_L,    KC_SCLN,  KC_ENT,
-  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,        KC_B,                                         KC_N,    KC_M,       KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-  KC_UNDS,  KC_EQL, KC_MINS,  KC_PLUS, MO(_LOWER),  LCTL_T(KC_SPC),                      RCTL_T(KC_SPC),  MO(_RAISE), KC_LCBR, KC_RCBR, KC_LBRC,  KC_RBRC,
-                                                KC_LALT,   KC_LGUI,                     KC_RGUI,      KC_RALT,
-                                            LT(_ADJUST, KC_PSCR),   KC_NXTWD,        KC_PRVWD,     KC_TML
+  KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,           KC_5,   KC_CAPS,                     KC_MUTE, KC_6,    KC_7,          KC_8,    KC_9,    KC_0,     KC_QUOT,
+  QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,           KC_T,   KC_PGUP,                     KC_VOLU, KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,     KC_BSPC,
+  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,           KC_G,   KC_PGDN,                     KC_VOLD, KC_H,    KC_J,          KC_K,    KC_L,    KC_SCLN,  KC_ENT,
+  KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,           KC_B,                                         KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
+  KC_UNDS,  KC_EQL, KC_MINS, KC_PLUS, KC_LOWER,  LCTL_T(KC_SPC),                      RCTL_T(KC_SPC),  KC_RAISE, KC_LCBR, KC_RCBR, KC_LBRC,  KC_RBRC,
+                                                KC_LALT,   KC_LGUI,                     KC_RGUI,      KC_ATL_QUOT,
+                                            KC_ADJ_PSCR,   KC_NXTWD,                    KC_PRVWD,     KC_TML
 ),
 
 /* LOWER
