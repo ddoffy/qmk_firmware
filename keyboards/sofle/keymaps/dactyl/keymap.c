ee88363da1c3b849969651d1b11263e9fa99e838
D. Doffy <cuongnsm@gmail.com>
2025-04-27 17:48:00 -0600

    Use thunb cluster as functiion for debug and test

--- keyboards/sofle/keymaps/dactyl/keymap.c
+++ keyboards/sofle/keymaps/dactyl/keymap.c
@@ -52,9 +52,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,     KC_G,   KC_PGDN,                     KC_VOLD, KC_H,    KC_J,          KC_K,    KC_L,    KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,                                         KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
   KC_UNDS,  KC_EQL, KC_LGUI, KC_LALT,           KC_LOWER, KC_SPC,                      KC_SPC,  KC_RAISE,                  KC_RALT, KC_RGUI, KC_LBRC,  KC_RBRC,
-                                                KC_F7,   KC_LCTL,                     KC_RCTL,      KC_F8,
-                                                KC_F9,KC_ADJ_PSCR,                    KC_TML,     KC_F10
-),
+                                                KC_F9,   KC_LCTL,                     KC_RCTL,      KC_F7,
+                                                KC_F10,KC_ADJ_PSCR,                    KC_TML,     KC_F8
+)
+    ,
 
 /* LOWER
  * ,-----------------------------------------.                    ,-----------------------------------------.
