0c9c3fa89dc7becdb2eded7cebb6076c4ca53469
D. Doffy <cuongnsm@gmail.com>
2025-04-27 17:42:45 -0600

    Change mod to ring finger and fuck figer

--- keyboards/sofle/keymaps/dactyl/keymap.c
+++ keyboards/sofle/keymaps/dactyl/keymap.c
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,   KC_PGUP,                     KC_VOLU, KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,     KC_BSPC,
   KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,     KC_G,   KC_PGDN,                     KC_VOLD, KC_H,    KC_J,          KC_K,    KC_L,    KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,                                         KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
-  KC_UNDS,  KC_EQL, KC_MINS, KC_PLUS,           KC_LOWER, KC_SPC,                      KC_SPC,  KC_RAISE,                  KC_LCBR, KC_RCBR, KC_LBRC,  KC_RBRC,
-                                                KC_LGUI,   KC_LCTL,                     KC_RCTL,      KC_RGUI,
-                                                KC_LALT,KC_ADJ_PSCR,                    KC_TML,     KC_RALT
+  KC_UNDS,  KC_EQL, KC_LGUI, KC_LALT,           KC_LOWER, KC_SPC,                      KC_SPC,  KC_RAISE,                  KC_RALT, KC_RGUI, KC_LBRC,  KC_RBRC,
+                                                KC_F7,   KC_LCTL,                     KC_RCTL,      KC_F8,
+                                                KC_F9,KC_ADJ_PSCR,                    KC_TML,     KC_F10
 ),
 
 /* LOWER
