26f45868cde1940f432b1ced0a929186512a06be
D. Doffy <cuongnsm@gmail.com>
2025-07-03 10:06:35 -0600

    Change config for mouse speed movement

--- keyboards/sofle/keymaps/dactyl/config.h
+++ keyboards/sofle/keymaps/dactyl/config.h
@@ -28,6 +28,34 @@
 #ifdef TAPPING_TERM
 #    undef TAPPING_TERM
 #endif
-#define TAPPING_TERM 200
+#define TAPPING_TERM 175
 
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
+
+
+// MOUSEKEY_DELAY	10	Delay between pressing a movement key and cursor movement
+// MOUSEKEY_INTERVAL	20	Time between cursor movements in milliseconds
+// MOUSEKEY_MOVE_DELTA	8	Step size
+// MOUSEKEY_MAX_SPEED	10	Maximum cursor speed at which acceleration stops
+// MOUSEKEY_TIME_TO_MAX	30	Time until maximum cursor speed is reached
+// MOUSEKEY_WHEEL_DELAY	10	Delay between pressing a wheel key and wheel movement
+// MOUSEKEY_WHEEL_INTERVAL	80	Time between wheel movements
+// MOUSEKEY_WHEEL_DELTA	1	Wheel movement step size
+// MOUSEKEY_WHEEL_MAX_SPEED	8	Maximum number of scroll steps per scroll action
+// MOUSEKEY_WHEEL_TIME_TO_MAX	40	Time until maximum scroll speed is reached
+
+#define MOUSEKEY_WHEEL_DELTA 3
+
+// MOUSEKEY_DELAY	10	Delay between pressing a movement key and cursor movement
+// MOUSEKEY_INTERVAL	20	Time between cursor movements in milliseconds
+// MOUSEKEY_MOVE_DELTA	8	Step size
+// MOUSEKEY_MAX_SPEED	10	Maximum cursor speed at which acceleration stops
+// MOUSEKEY_TIME_TO_MAX	30	Time until maximum cursor speed is reached
+// MOUSEKEY_WHEEL_DELAY	10	Delay between pressing a wheel key and wheel movement
+// MOUSEKEY_WHEEL_INTERVAL	80	Time between wheel movements
+// MOUSEKEY_WHEEL_DELTA	1	Wheel movement step size
+// MOUSEKEY_WHEEL_MAX_SPEED	8	Maximum number of scroll steps per scroll action
+// MOUSEKEY_WHEEL_TIME_TO_MAX	40	Time until maximum scroll speed is reached
+
+#define MOUSEKEY_MOVE_DELTA 2 // Step size
+#define MOUSEKEY_MAX_SPEED 5  // Maximum cursor speed at which acceleration stops
