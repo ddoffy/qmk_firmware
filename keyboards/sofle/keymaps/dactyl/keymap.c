14920bdaf75e0374d95e289b6826e5db54b7cb25
D. Doffy <cuongnsm@gmail.com>
2026-07-29 12:01:30 -0600

    new key map for my hyper key with tab

--- keyboards/sofle/keymaps/dactyl/keymap.c
+++ keyboards/sofle/keymaps/dactyl/keymap.c
@@ -9,6 +9,8 @@
 #define KC_LOWER MO(_LOWER)
 #define KC_LCTL LCTL_T(KC_SPC)
 #define KC_RCTL RCTL_T(KC_SPC)
+// Tap for Tab, hold for Hyper (Ctrl+Shift+Alt+Super) -- for KDE global shortcuts
+#define KC_HYPR_TAB HYPR_T(KC_TAB)
 
 // const uint16_t PROGMEM esc_combo[] = {KC_A, KC_S, COMBO_END};
 // combo_t key_combos[] = {
@@ -40,7 +42,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
  * | ESC  |   Q  |   W  |   E  |   R  |   T  |                    |   Y  |   U  |   I  |   O  |   P  | Bspc |
  * |------+------+------+------+------+------|                    |------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
+ * |Tab/Hy|   A  |   S  |   D  |   F  |   G  |-------.    ,-------|   H  |   J  |   K  |   L  |   ;  |  '   |
  * |------+------+------+------+------+------|  MUTE |    |       |------+------+------+------+------+------|
  * |LShift|   Z  |   X  |   C  |   V  |   B  |-------|    |-------|   N  |   M  |   ,  |   .  |   /  |RShift|
  * `-----------------------------------------/       /     \      \-----------------------------------------'
@@ -52,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_QWERTY] = LAYOUT(
   KC_GRV,   KC_1,   KC_2,    KC_3,    KC_4,     KC_5,   KC_CAPS,                     KC_MUTE, KC_6,    KC_7,          KC_8,    KC_9,    KC_0,     KC_QUOT,
   QK_GESC,  KC_Q,   KC_W,    KC_E,    KC_R,     KC_T,   KC_PGUP,                     KC_VOLU, KC_Y,    KC_U,          KC_I,    KC_O,    KC_P,     KC_BSPC,
-  KC_TAB,   KC_A,   KC_S,    KC_D,    KC_F,     KC_G,   KC_PGDN,                    KC_VOLD, KC_H,    KC_J,          KC_K,    KC_L,    KC_SCLN,  KC_ENT,
+  KC_HYPR_TAB, KC_A, KC_S,   KC_D,    KC_F,     KC_G,   KC_PGDN,                    KC_VOLD, KC_H,    KC_J,          KC_K,    KC_L,    KC_SCLN,  KC_ENT,
   KC_LSFT,  KC_Z,   KC_X,    KC_C,    KC_V,     KC_B,                                         KC_N,    KC_M,          KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT,
   KC_UNDS,  KC_EQL, KC_LGUI, KC_LALT,           KC_LOWER, KC_LCTL,               KC_RCTL,  KC_RAISE,                  KC_RALT, KC_RGUI, KC_LBRC,  KC_RBRC,
                                                 KC_ADJ_PSCR, KC_F5,                 KC_F6, KC_TML, 
